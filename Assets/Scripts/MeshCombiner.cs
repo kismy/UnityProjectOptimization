c1d3445b5e079d1e737161199b31c74cb9d89c1d
panzehong <2917179196@qq.com>
2019-11-29 15:37:48 +0800

    1

---
+++ Assets/Scripts/MeshCombiner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshCombiner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MeshCombine();
+	}
+	
+    void MeshCombine()
+    {
+        MeshFilter[] filters = GetComponentsInChildren<MeshFilter>();
+
+        CombineInstance[] combiners = new CombineInstance[filters.Length];
+
+        for(int i = 0; i < filters.Length; i++)
+        {
+            combiners[i].mesh = filters[i].sharedMesh;
+            combiners[i].transform = filters[i].transform.localToWorldMatrix;
+        }
+
+        Mesh finalMesh = new Mesh();
+        finalMesh.CombineMeshes(combiners);
+
+        GetComponent<MeshFilter>().sharedMesh = finalMesh;
+    }
+}
